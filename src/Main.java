c79e51217ebf481b653ab417ff98e02a4d3e471c
Marta Lima <121465689+marta-lima-santos@users.noreply.github.com>
2023-06-03 15:10:29 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,26 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        String day = "Friday";
+        switch (day){
+            case "Sunday":
+                System.out.println("Today is Sunday");
+                break;
+            case "Monday":
+                System.out.println("Today is Monday");
+                break;
+            case "Tuesday":
+                System.out.println("Today is Tuesday");
+            case "Wednesday":
+                System.out.println("Today is Wednesday");
+                break;
+            case "Thursday":
+                System.out.println("Today is Thursday");
+                break;
+            case "Friday":
+                System.out.println("Today is Friday");
+                break;
+            default:
+                System.out.println("Please, enter the correct day");
+        }
         }
     }
-}
